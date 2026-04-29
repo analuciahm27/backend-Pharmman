2b72304ab338845bed3bd578ede8a47a70413112
Andrea <anjolu312@gmail.com>
2026-04-28 21:45:42 -0500

    fix: agregar import faltante de DisabledException

--- src/main/java/com/pharmman/backend/controller/AuthController.java
+++ src/main/java/com/pharmman/backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
